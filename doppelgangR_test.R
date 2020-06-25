3b921d3f2acee87d50dde5c31dc95c9307634f1f
mikebocker <66046568+mikebocker@users.noreply.github.com>
2020-06-25 09:30:07 -0500

    Update doppelgangR_test.R

--- doppelgangR_test.R
+++ doppelgangR_test.R
@@ -11,9 +11,12 @@ GSE26906_exprs <- read.csv("GSE26906_exprs.csv")
 GSE39582_exprs <- read.csv("GSE39582_exprs.csv")
 
 # Create list of type ExpressionSet
-GSE26906_eset <- makeExpressionSet(GSE26906_exprs, state = colnames(GSE26906_exprs))
-GSE39582_eset <- makeExpressionSet(GSE39582_exprs, state = colnames(GSE39582_exprs))
+GSE26906_mat = data.matrix(GSE26906_exprs)
+GSE39582_mat = data.matrix(GSE39582_exprs)
 
+# Create list of type ExpressionSet
+GSE26906_eset <- ExpressionSet(assayData = GSE26906_mat)
+GSE39582_eset <- ExpressionSet(assayData = GSE39582_mat)
 
 # Run doppelgangR on datasets
 # Expected number of duplicates: 90 (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5241903/)
